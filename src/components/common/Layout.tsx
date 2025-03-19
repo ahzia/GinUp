4f53ea7040a781b1889f260f45392d094a5a17bb
ahzamir <ahmadzamiryousofi1397@gmail.com>
2025-03-19 20:48:33 +0100

    added the main layout for the all pages

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <div className="min-h-screen flex flex-col items-baseline">
+            <header>
+                <div className="container mx-auto">
+                    {/* Navbar place */}
+                    Navbar
+                </div>
+            </header>
+
+            <main className="container pb-8">{children}</main>
+        </div>
+    );
+}
